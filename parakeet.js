1fc0ca515a63c6039890d750002f09fd344a1c6b
John Xia <john.danger.xia@gmail.com>
2015-05-08 05:33:01 -0500

    weird no worky

--- parakeet.js
+++ parakeet.js
@@ -107,7 +107,7 @@ function get_item(itemnum){
 
 function complete_item(){
   point_value = Number($("#pointval").val());
-  if (point_value < 0 || isNan(point_value)) {
+  if (point_value < 0 || isNaN(point_value)) {
     $("#woohoo").html("Please enter a valid point value :(");
     $("#woohoo").fadeIn();
     window.setTimeout(function(){$("#woohoo").fadeOut();}, 800);
